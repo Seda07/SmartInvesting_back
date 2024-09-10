80e51483eb03fbafdeaa42abd3e6ec5e2fcc05de
Seda07 <gevorgyanseda0@gmail.com>
2024-09-10 09:50:43 +0200

    DONE: last commit

--- users/views.py
+++ users/views.py
@@ -1,24 +1,29 @@
+from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
-from django.contrib.auth import authenticate
 from .serializers import RegisterUserSerializer, TokenSerializer
 
-# Vista de registro de usuario
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = RegisterUserSerializer(data=request.data)
         if serializer.is_valid():
-            # Guarda el usuario en la base de datos
-            serializer.save()
-            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            token_serializer = TokenSerializer({
+                'access': str(refresh.access_token),
+                'refresh': str(refresh)
+            })
+            return Response({
+                'message': 'User registered successfully',
+                'tokens': token_serializer.data
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Vista de inicio de sesión con generación de tokens JWT
 class LoginView(APIView):
     permission_classes = [AllowAny]
 
@@ -26,14 +31,14 @@ class LoginView(APIView):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
-
         if user is not None:
-            # Generar tokens de acceso y refresco
             refresh = RefreshToken.for_user(user)
             token_serializer = TokenSerializer({
                 'access': str(refresh.access_token),
                 'refresh': str(refresh)
             })
-            return Response(token_serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({
+                'message': 'Login successful',
+                'tokens': token_serializer.data
+            }, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
